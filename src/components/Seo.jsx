e7550ca72aca47c5be14d7fc2241e8c298f3ecb5
daehyeong2 <baconbacon1231@gmail.com>
2024-05-02 23:46:26 +0900

    SEO 설정

--- src/components/Seo.jsx
+++ src/components/Seo.jsx
@@ -7,20 +7,22 @@ const Seo = ({
   return (
     <Helmet>
       <title>{title} | ArtifyThumbs</title>
+      <meta property="og:type" content="website" />
       <meta property="og:title" content={`${title} | ArtifyThumbs`}></meta>
+      <meta property="og:site_name" content="ArtifyThumbs" />
       <meta name="og:description" content={description} />
       <meta property="og:type" content="website" />
-      <meta property="og:image" content="/img/smallLogo.jpeg"></meta>
       <meta name="description" content={description} />
+      <meta property="og:locale" content="kr_ko" />
+      <meta property="og:url" content="https://artifythumbs.netlify.app/" />
       <meta
         name="keywords"
         content="그림, 간편한, 깔끔한, 빠른, 친절한, 유튜브 썸네일, 프로필 사진, 프로필 배너, 게임 일러스트, 매드무비 썸네일, 게임 썸네일"
       />
       <link rel="canonical" href="http://artifythumbs.netlify.app" />
-      <meta
-        property="og:image"
-        content="https://web-artifythumbs-frontend-754g42alusa19vw.sel5.cloudtype.app/img/smallLogo.jpeg"
-      />
+      <meta property="og:image" content="/img/smallLogo.jpeg" />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
     </Helmet>
   );
 };
